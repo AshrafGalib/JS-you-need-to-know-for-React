a04216b39aead3f934c952dfc51e5bfc83cd3430
AshrafGalib <108192630+AshrafGalib@users.noreply.github.com>
2024-11-21 15:57:24 +0600

    Add files via upload

---
+++ template_string.js
@@ -0,0 +1,9 @@
+const age=[20,21,22,23,24,25,26,27,28,29,30]
+const student ={
+    name : 'Ashraf Galib',
+    dept : 'CSE',
+    brothers : ['Wasif','Farhan','Mahtab','Hasnat','Ucchash','Safrat','Rocky','Sajjad','Jabed']
+
+}
+const about =`I am ${student.name}. I'm studying on ${student.dept}.My age is ${age[4]} years. My brother, ${student.brothers[1]} is a Spiderman.`
+console.log(about)
