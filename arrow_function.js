9fe922ac7571ff959a8e5b61d6a717c52f52100d
AshrafGalib <108192630+AshrafGalib@users.noreply.github.com>
2024-11-21 16:11:58 +0600

    Add files via upload

---
+++ arrow_function.js
@@ -0,0 +1,13 @@
+const sum=(x,y)=>x+y
+const result =sum(9,5);
+console.log(result)
+
+//multiline
+
+const calculate = x =>{
+    let numb = x+45
+    let result = numb*2
+    return result;
+}
+const CallFunc =calculate(2)
+console.log(CallFunc)
