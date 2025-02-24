1efba48036a0e388b8fa0cbb276a3506f6ade22b
AshrafGalib <108192630+AshrafGalib@users.noreply.github.com>
2025-02-24 17:13:06 +0600

    Add files via upload

---
+++ array_method_find.js
@@ -0,0 +1,9 @@
+const electronicAssets =[
+    {name:'Laptop',brand:'Walton',prize:78515},
+    {name:'Phone',brand:'Apple',prize:69990},
+    {name:'PC',brand:'Gygabyte',prize:80000},
+    {name:'Vape',brand:'Argus',prize:5000},
+]
+
+const selectedPrices = electronicAssets.find(p=>p.prize > 70000)
+console.log(selectedPrices)
---
+++ array_method_forEach.js
@@ -0,0 +1,8 @@
+const electronicAssets =[
+    {name:'Laptop',brand:'Walton',prize:78515},
+    {name:'Phone',brand:'Apple',prize:69990},
+    {name:'PC',brand:'Gygabyte',prize:80000},
+    {name:'Vape',brand:'Argus',prize:5000},
+]
+electronicAssets.forEach(name=>console.log(name.brand))
+// map doesn't return
