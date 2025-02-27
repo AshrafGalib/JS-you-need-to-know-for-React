1afddb094c7de06067e6d1f43e46d8c00d652919
AshrafGalib <108192630+AshrafGalib@users.noreply.github.com>
2025-02-27 15:54:58 +0600

    Add files via upload

---
+++ array_destructuring.js
@@ -0,0 +1,9 @@
+const UsingDevices = ['Laptop','PC','Argus','Cell phone','Watch']
+const [firstDevice,secondDevice,thirdDevice,forthDevice,fifthDevice]=UsingDevices;
+console.log(`My ${forthDevice}'s model name is Apple and my ${fifthDevice}'s model name is Poedagar.`)
+
+const makeArray =(x,y) =>[x,y]
+const array = makeArray(45,89)
+
+const [firstNumber,secondNumber]= array;
+console.log(secondNumber)
